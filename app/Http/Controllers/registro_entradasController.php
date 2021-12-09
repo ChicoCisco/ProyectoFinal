9e229d6546f11475748f235a72c3f4683b51e273
Mariano Rodriguez <51105402+ChicoCisco@users.noreply.github.com>
2021-12-09 16:16:25 -0600

    Update registro_entradasController.php

--- app/Http/Controllers/registro_entradasController.php
+++ app/Http/Controllers/registro_entradasController.php
@@ -11,7 +11,7 @@ class registro_entradasController extends Controller
 {
     public function ChecarExistencia()
     {
-        $response = Http::get('https://io.adafruit.com/api/v2/CarlosLpz/feeds/codigosnfc/data?limit=1&X-AIO-Key=aio_qhQi79wEQsIQnJSY9jIqnnj6Nyl8');
+        $response = Http::get('https://io.adafruit.com/api/v2/CarlosLpz/feeds/codigosnfc/data?limit=1&X-AIO-Key=aio_yuzQ94Ha5HqamY60HULORpX6fZBZ');
         $posts=json_decode($response->body());
 
         $postuser1 = array();
@@ -32,32 +32,27 @@ class registro_entradasController extends Controller
 
                 $respuesta='1';
 
-                $cl = array(
-                    'headers' => [ 'Content-Type' => 'multipart/form-data' ],
-                    //'body' => json_encode(['value' => '1'])
-                );
-
-                $body = array(
-                    'value' => '1'
-                );
-
-                $response2=Http::Post('https://io.adafruit.com/api/v2/CarlosLpz/feeds/nfcactivado/data?X-AIO-Key=aio_OSaX270JDoA6efw0vhef8a9RxoLt', $cl, $body);
+                $response = Http::withHeaders([
+                    'X-AIO-Key' => 'aio_yuzQ94Ha5HqamY60HULORpX6fZBZ'
+                ])->post('https://io.adafruit.com//api/v2/CarlosLpz/feeds/nfcactivado/data', [
+                    'value' => "1",
+                ]);
             }
 
-            else
-            {
-                $respuesta='-';
-            }
+            //else
+            //{
+            //    $respuesta='-';
+            //}
         }
 
         /*echo json_encode($postuser1);*/
-        return $respuesta;
+        //return $respuesta;
     }
     public function ConsultarEntradas()
     {
         $insertar = new registro_entradas();
 
-        $response = Http::get('https://io.adafruit.com/api/v2/CarlosLpz/feeds/codigosnfc/data?limit=10&X-AIO-Key=aio_OSaX270JDoA6efw0vhef8a9RxoLt');
+        $response = Http::get('https://io.adafruit.com/api/v2/CarlosLpz/feeds/codigosnfc/data?limit=10&X-AIO-Key=aio_qhQi79wEQsIQnJSY9jIqnnj6Nyl8');
         $posts=json_decode($response->body());
 
         $postuser1 = array();
